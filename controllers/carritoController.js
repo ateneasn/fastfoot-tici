a284a4dc60b071787bbceab2f8406645b1bc5a73
Tici <ateneasn@gmail.com>
2022-07-06 21:27:52 -0300

    Subida de estructura base

---
+++ controllers/carritoController.js
@@ -0,0 +1,20 @@
+module.exports = {
+    addProduct: (req,res) => {
+        //
+    },
+    deleteProduct: (req,res) => {
+        //
+    },
+    cantModify: (req,res) => {
+        //
+    },
+    checkDiscount: (req,res) => {
+        //
+    },
+    confirmCompra: (req,res) => {
+        //
+    },
+    emptyCarrito: (req,res) => {
+        //
+    },
+};
---
+++ controllers/productsController.js
@@ -0,0 +1,11 @@
+module.exports = {
+    listProducts: (req,res) => {
+        //
+    },
+    getProduct: (req,res) => {
+        //
+    },
+    searchProduct: (req,res) => {
+
+    },
+}
